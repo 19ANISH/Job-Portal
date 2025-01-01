5de0c8bcd5aae19422eeda74c8f41a06de60e42a
AnishRajwani <anish.rajwani@whiteklay.in>
2025-01-01 14:12:46 +0530

    added new dependncy of table

--- db_models.py
+++ db_models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column,Integer,String,TIMESTAMP, Text, DATE
-from database import Base
+from sqlalchemy import Column,Integer,String,TIMESTAMP, Text, DATE,func
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
 class admin(Base):
     __tablename__ = 'admin_table'
 
     id = Column(Integer,primary_key=True)
+    created =  Column(DATE, default=func.current_date())
+    email = Column(String(150),unique=True)
     username = Column(String(100),unique=True)
     password = Column(String(500))
     token = Column(Text)
@@ -12,14 +16,16 @@ class admin(Base):
 class CompanyDetails(Base):
     __tablename__ = 'details'
 
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    company = Column(String(100))
-    designation = Column(String(100))
-    description= Column(String)
-    image = Column(String)
-    updated_date = Column(DATE, default="CURRENT_DATE")  # Defaults to current date
-    inactive_date = Column(DATE)  # This can be NULL
-    application = Column(String(150))
-    salary = Column(String(20))
-    batch = Column(String(25))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    location = Column(String(100), default="")
+    companyName = Column(String(100))  # String with a max length of 100
+    designation = Column(String(100))  # String with a max length of 100
+    description = Column(String(255))  # VARCHAR(255) for MySQL
+    image = Column(String(255))  # String with a max length of 255 (for image URLs or file names)
+    created =  Column(DATE, default=func.current_date())  # Defaults to current date
+    deadline = Column(DATE)  # This can be NULL
+    applicationLink = Column(String(150))  # String with a max length of 150
+    salary = Column(String(20))  # String with a max length of 20
+    batch = Column(String(25))  # String with a max length of 25
+
 
